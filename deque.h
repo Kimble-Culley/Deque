5d849a663f12c75a4ed6111a1488190ee80b2eca
Sydney Marshall <tmq265@ut.utm.edu>
2024-04-29 19:01:25 -0500

    documentation done to .h and main

--- deque.h
+++ deque.h
@@ -1,8 +1,19 @@
+/**
+ * @file deque.h
+ * @author Kimble Culley and Sydney Marshall
+ * @date 2024-04-29
+ * @brief declares deque class 
+ * 
+ * declares deque class and member variables and methods 
+ */
+
+
 #ifndef DEQUE_H
 #define DEQUE_H
 
 class Deque{
  private:
+  // pointer to a pointer 
   int** blockmap;
   int frontBlock;
   int backBlock;
@@ -12,23 +23,87 @@ class Deque{
   int blockSize;
   int numElements;
 
-  void expandFront();
-  void expandBack();
-  void shrinkFront();
-  void shrinkBack();
+
+  void expandFront();//expands deque towards front 
+  void expandBack();//expands deque towards back
+  void shrinkFront();//shrinks deque towards front
+  void shrinkBack();//shrinks deque towards back 
 
  public:
+
+/**
+ * constructor to initialize deque 
+ * @post deque is initialized 
+ */
   Deque();
+
+/**
+ * destructor
+ * @post memory is deallocated
+ */
   ~Deque();
 
+
+/**
+ * adds element to front of deque
+ * @param int item 
+ * @return int 
+ * @post element is added to the front
+ */
   int push_front(int item);
+
+/**
+ * adds element to the back of deque
+ * @param int item 
+ * @return int 
+ * @post element is added to the back
+ */
   int push_back(int item);
+
+/**
+ * removes and returns element from front of deque
+ * @return int 
+ * @post element is removed from front  
+ */
   int pop_front();
+
+/**
+ * removes and returns element from back
+ * @return int 
+ * @post element is removed from back 
+ */
   int pop_back();
+
+/**
+ * returns element at front of deque
+ * @return int  
+ */
   int front();
+
+/**
+ * returns element at back of deque
+ * @return int  
+ */
   int back();
+
+/**
+ * checks if deque is empty
+ * @return bool  
+ */
   bool empty();
+
+/**
+ * returns number of elements in deque
+ * @return int  
+ */
   int size();
+
+/**
+ * overloaded subscript op. to access elements by index
+ * @param int index 
+ * @pre index is within range
+ * @return int  
+ */
   int operator[](int index);
 
 };
--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 /**
-0;10;1c * @file main.cpp
- * @author Kimble Culley
+   @file main.cpp
+ * @author Kimble Culley and Sydney Marshall
  * @date 2024-04-23
- * @brief idk
+ * @brief driver program
  * 
- * idk
+ * driver program for the implementation of the deque. it shows how the data in the deque
+can be added, removed, and accessed. 
  */
 
 #include "deque.h"
@@ -14,29 +15,34 @@ using namespace std;
 
 int main() {
 
-  Deque deque;
-  int counter = 0;
-  
+  Deque deque; // deque class
+  int counter = 0; // keeps track of data added to the deque 
+
+  // adds data to the back of deque and prints them 
   for(int i = 0; i < 1000; i++){
     deque.push_back(i);
-    cout << " " << deque[i];
-    counter++;
+    cout << " " << deque[i]; // this prints new data 
+    counter++; 
   }
 
+  // removes data from the front of the deque 
   for(int i = 0; i < 100; i++){
     deque.pop_front();
-    counter--;
+    counter--;  
   }
 
+  // prints data that remains in the deque 
   for(int i = 0; i < counter; i++){
     cout << " " << deque[i];
   }
 
+  // removes data from the back of the deque 
   for(int i = 0; i < 100; i++){
     deque.pop_back();
-    counter--;
+    counter--; 
   }
 
+  // prints data that remains in deque 
   for(int i = 0; i < counter; i++){
     cout << " " << deque[i];
   }
